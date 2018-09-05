9028f614548a5a5c949d9213d667a4df69f3eb6a
An <30442559+epaqu@users.noreply.github.com>
2018-09-05 18:12:46 +0900

    classwork

---
+++ Documentation and Strings/Documentation and Strings/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn1 = New System.Windows.Forms.Button
+        Me.btn2 = New System.Windows.Forms.Button
+        Me.btn3 = New System.Windows.Forms.Button
+        Me.lstbox = New System.Windows.Forms.ListBox
+        Me.txtbox1 = New System.Windows.Forms.TextBox
+        Me.txtbox2 = New System.Windows.Forms.TextBox
+        Me.SuspendLayout()
+        '
+        'btn1
+        '
+        Me.btn1.Location = New System.Drawing.Point(12, 12)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(75, 23)
+        Me.btn1.TabIndex = 0
+        Me.btn1.Text = "-="
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.Location = New System.Drawing.Point(109, 12)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(75, 23)
+        Me.btn2.TabIndex = 1
+        Me.btn2.Text = "Button2"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.Location = New System.Drawing.Point(205, 12)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(75, 23)
+        Me.btn3.TabIndex = 2
+        Me.btn3.Text = "Button3"
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'lstbox
+        '
+        Me.lstbox.FormattingEnabled = True
+        Me.lstbox.Location = New System.Drawing.Point(89, 98)
+        Me.lstbox.Name = "lstbox"
+        Me.lstbox.Size = New System.Drawing.Size(120, 95)
+        Me.lstbox.TabIndex = 3
+        '
+        'txtbox1
+        '
+        Me.txtbox1.Location = New System.Drawing.Point(12, 56)
+        Me.txtbox1.Name = "txtbox1"
+        Me.txtbox1.Size = New System.Drawing.Size(100, 20)
+        Me.txtbox1.TabIndex = 4
+        '
+        'txtbox2
+        '
+        Me.txtbox2.Location = New System.Drawing.Point(180, 56)
+        Me.txtbox2.Name = "txtbox2"
+        Me.txtbox2.Size = New System.Drawing.Size(100, 20)
+        Me.txtbox2.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(816, 384)
+        Me.Controls.Add(Me.txtbox2)
+        Me.Controls.Add(Me.txtbox1)
+        Me.Controls.Add(Me.lstbox)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btn1 As System.Windows.Forms.Button
+    Friend WithEvents btn2 As System.Windows.Forms.Button
+    Friend WithEvents btn3 As System.Windows.Forms.Button
+    Friend WithEvents lstbox As System.Windows.Forms.ListBox
+    Friend WithEvents txtbox1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtbox2 As System.Windows.Forms.TextBox
+
+End Class
---
+++ Documentation and Strings/Documentation and Strings/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtbox1.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn1.Click
+        Dim intNum As Integer
+        intNum = CInt(txtbox1.Text)
+        lstbox.Items.Add(intNum * 6)
+
+    End Sub
+
+    Private Sub btn2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn2.Click
+        Dim StrOne, StrTwo As String
+        Dim intResult As Integer
+        StrOne = txtbox1.Text
+        StrTwo = txtbox2.Text
+        intResult = StrOne.IndexOf(StrTwo)
+        lstbox.Items.Add(intResult)
+
+    End Sub
+
+    Private Sub btn3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn3.Click
+        Dim StrOne As String
+        StrOne = txtbox1.Text
+        lstbox.Items.Add(StrOne.Substring(2, 3))
+
+    End Sub
+End Class
