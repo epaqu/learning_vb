3e866e822eb8be6aa7478d722cbceed4f6936961
An <30442559+epaqu@users.noreply.github.com>
2018-09-05 17:28:50 +0900

    assignment 1

---
+++ AnTest1/AnTest1/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnred = New System.Windows.Forms.Button
+        Me.btnwhite = New System.Windows.Forms.Button
+        Me.btnblue = New System.Windows.Forms.Button
+        Me.btngreen = New System.Windows.Forms.Button
+        Me.btndis1 = New System.Windows.Forms.Button
+        Me.txt1 = New System.Windows.Forms.TextBox
+        Me.btndis2 = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'btnred
+        '
+        Me.btnred.Location = New System.Drawing.Point(38, 45)
+        Me.btnred.Name = "btnred"
+        Me.btnred.Size = New System.Drawing.Size(75, 23)
+        Me.btnred.TabIndex = 0
+        Me.btnred.Text = "Red"
+        Me.btnred.UseVisualStyleBackColor = True
+        '
+        'btnwhite
+        '
+        Me.btnwhite.Location = New System.Drawing.Point(38, 75)
+        Me.btnwhite.Name = "btnwhite"
+        Me.btnwhite.Size = New System.Drawing.Size(75, 23)
+        Me.btnwhite.TabIndex = 1
+        Me.btnwhite.Text = "White"
+        Me.btnwhite.UseVisualStyleBackColor = True
+        '
+        'btnblue
+        '
+        Me.btnblue.Location = New System.Drawing.Point(167, 45)
+        Me.btnblue.Name = "btnblue"
+        Me.btnblue.Size = New System.Drawing.Size(75, 23)
+        Me.btnblue.TabIndex = 2
+        Me.btnblue.Text = "Blue"
+        Me.btnblue.UseVisualStyleBackColor = True
+        '
+        'btngreen
+        '
+        Me.btngreen.Location = New System.Drawing.Point(167, 75)
+        Me.btngreen.Name = "btngreen"
+        Me.btngreen.Size = New System.Drawing.Size(75, 23)
+        Me.btngreen.TabIndex = 3
+        Me.btngreen.Text = "Green"
+        Me.btngreen.UseVisualStyleBackColor = True
+        '
+        'btndis1
+        '
+        Me.btndis1.Location = New System.Drawing.Point(38, 175)
+        Me.btndis1.Name = "btndis1"
+        Me.btndis1.Size = New System.Drawing.Size(75, 23)
+        Me.btndis1.TabIndex = 4
+        Me.btndis1.Text = "Disappear"
+        Me.btndis1.UseVisualStyleBackColor = True
+        '
+        'txt1
+        '
+        Me.txt1.Location = New System.Drawing.Point(38, 127)
+        Me.txt1.Name = "txt1"
+        Me.txt1.Size = New System.Drawing.Size(204, 20)
+        Me.txt1.TabIndex = 5
+        '
+        'btndis2
+        '
+        Me.btndis2.Location = New System.Drawing.Point(167, 175)
+        Me.btndis2.Name = "btndis2"
+        Me.btndis2.Size = New System.Drawing.Size(75, 23)
+        Me.btndis2.TabIndex = 6
+        Me.btndis2.Text = "Disappear"
+        Me.btndis2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(292, 266)
+        Me.Controls.Add(Me.btndis2)
+        Me.Controls.Add(Me.txt1)
+        Me.Controls.Add(Me.btndis1)
+        Me.Controls.Add(Me.btngreen)
+        Me.Controls.Add(Me.btnblue)
+        Me.Controls.Add(Me.btnwhite)
+        Me.Controls.Add(Me.btnred)
+        Me.Name = "Form1"
+        Me.Text = "An"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnred As System.Windows.Forms.Button
+    Friend WithEvents btnwhite As System.Windows.Forms.Button
+    Friend WithEvents btnblue As System.Windows.Forms.Button
+    Friend WithEvents btngreen As System.Windows.Forms.Button
+    Friend WithEvents btndis1 As System.Windows.Forms.Button
+    Friend WithEvents txt1 As System.Windows.Forms.TextBox
+    Friend WithEvents btndis2 As System.Windows.Forms.Button
+
+End Class
---
+++ AnTest1/AnTest1/Form1.vb
@@ -0,0 +1,66 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        btndis2.Visible = False
+    End Sub
+
+    Private Sub btnred_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnred.Click
+        btnred.BackColor = Color.Red
+        btnblue.BackColor = Color.Red
+        btngreen.BackColor = Color.Red
+        btnwhite.BackColor = Color.Red
+        btndis1.BackColor = Color.Red
+        btndis2.BackColor = Color.Red
+    End Sub
+
+    Private Sub btnwhite_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnwhite.Click
+        Me.BackColor = Color.White
+    End Sub
+
+    Private Sub btnblue_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnblue.Click
+        btnred.ForeColor = Color.Blue
+        btndis1.ForeColor = Color.Blue
+        btnblue.ForeColor = Color.Blue
+        btngreen.ForeColor = Color.Blue
+        btnwhite.ForeColor = Color.Blue
+    End Sub
+
+    Private Sub btngreen_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btngreen.Click
+        btnred.ForeColor = Color.Green
+        btnblue.ForeColor = Color.Green
+        btngreen.ForeColor = Color.Green
+        btndis1.ForeColor = Color.Green
+        btndis2.ForeColor = Color.Green
+        btnwhite.ForeColor = Color.Green
+    End Sub
+
+    Private Sub Txt1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txt1.TextChanged
+        btnred.BackColor = Color.LightGray
+        btnwhite.BackColor = Color.LightGray
+        btngreen.BackColor = Color.LightGray
+        btnblue.BackColor = Color.LightGray
+        btndis1.BackColor = Color.LightGray
+        btndis2.BackColor = Color.LightGray
+        Me.BackColor = Color.Yellow
+        btnred.ForeColor = Color.White
+        btndis1.ForeColor = Color.White
+        btndis2.ForeColor = Color.White
+        btnblue.ForeColor = Color.White
+        btngreen.ForeColor = Color.White
+        btnwhite.ForeColor = Color.White
+    End Sub
+    Private Sub Txt1_leave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txt1.Leave
+        txt1.Clear()
+    End Sub
+
+    Private Sub btndis1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btndis1.Click
+        btndis1.Visible = False
+        btndis2.Visible = True
+    End Sub
+
+    Private Sub btndis2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btndis2.Click
+        btndis2.Visible = False
+        btndis1.Visible = True
+    End Sub
+End Class
+
