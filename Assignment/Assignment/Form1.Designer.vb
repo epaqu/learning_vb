a5cb1b411c9f5675e838eec19ce8d1601babd283
An <30442559+epaqu@users.noreply.github.com>
2018-09-05 17:30:19 +0900

    assignment 3

---
+++ Assignment/Assignment/Form1.Designer.vb
@@ -0,0 +1,69 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txt1 = New System.Windows.Forms.TextBox
+        Me.txt2 = New System.Windows.Forms.TextBox
+        Me.txt3 = New System.Windows.Forms.TextBox
+        Me.SuspendLayout()
+        '
+        'txt1
+        '
+        Me.txt1.Location = New System.Drawing.Point(87, 52)
+        Me.txt1.Name = "txt1"
+        Me.txt1.Size = New System.Drawing.Size(21, 20)
+        Me.txt1.TabIndex = 2
+        '
+        'txt2
+        '
+        Me.txt2.Location = New System.Drawing.Point(87, 78)
+        Me.txt2.Name = "txt2"
+        Me.txt2.Size = New System.Drawing.Size(21, 20)
+        Me.txt2.TabIndex = 1
+        '
+        'txt3
+        '
+        Me.txt3.Location = New System.Drawing.Point(87, 104)
+        Me.txt3.Name = "txt3"
+        Me.txt3.Size = New System.Drawing.Size(21, 20)
+        Me.txt3.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(292, 266)
+        Me.Controls.Add(Me.txt3)
+        Me.Controls.Add(Me.txt2)
+        Me.Controls.Add(Me.txt1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txt1 As System.Windows.Forms.TextBox
+    Friend WithEvents txt2 As System.Windows.Forms.TextBox
+    Friend WithEvents txt3 As System.Windows.Forms.TextBox
+
+End Class
---
+++ Assignment/Assignment/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+
+    Private Sub txt3_leave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txt3.Leave
+        txt3.BackColor = Color.DarkGray
+        txt2.BackColor = Color.Yellow
+        
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        txt3.BackColor = Color.Green
+        txt2.BackColor = Color.DarkGray
+        txt1.BackColor = Color.DarkGray
+
+    End Sub
+
+    Private Sub txt2_leave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txt2.Leave
+        txt2.BackColor = Color.DarkGray
+        txt1.BackColor = Color.Red
+    End Sub
+
+    Private Sub txt1_leave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txt1.Leave
+        txt1.BackColor = Color.DarkGray
+        txt3.BackColor = Color.Green
+    End Sub
+End Class
